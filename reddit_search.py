0b77ee7650bec15c05d9e0c90d0f8e64d9eb03a2
Saturnist <jlau181@gmail.com>
2017-02-12 16:04:20 -0800

    filter out useless reddit urls

--- reddit_search.py
+++ reddit_search.py
@@ -2,12 +2,16 @@ import praw
 
 
 def reddit_search(sub, start):
+    blacklist = [ 'reddit.com', 'redd.it', 'i.reddituploads.com']
     results = {}
     reddit = praw.Reddit()
     subreddit = reddit.subreddit(sub)
 
     for d in start:
         for b in subreddit.submissions(d, d + 86400):
-            results[b.title] = b.url
+            if any(reddit_url in b.url for reddit_url in blacklist):
+                continue
+            else:
+                results[b.title] = b.url
 
     return results
